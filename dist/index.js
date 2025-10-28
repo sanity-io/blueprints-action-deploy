061187b79d57195bf53cb082ef7499279b0d6cff
Stephen Fisher <stephen.fisher@sanity.io>
2025-10-28 17:45:17 -0600

    getting there

--- dist/index.js
+++ dist/index.js
@@ -27555,39 +27555,47 @@ module.exports = parseParams
 /******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
-const core = __nccwpck_require__(7484);
-const exec = __nccwpck_require__(5236);
+const core = __nccwpck_require__(7484)
+const exec = __nccwpck_require__(5236)
 
-// In the runtime cli it assumes the blueprints file is in the root and available.
-// Can the runtime cli take a path to the blueprints file? If so we can make that an input here too.
+// The runtime cli assumes the blueprints file is in the root and available.
+// Does the runtime cli eventually need to be extended to allow for additional vars?
+// A file path could be input if users need to define where their blueprints file is located, and then probably set the working directory too.
 
+// For now, this action will just run `runtime-cli blueprints deploy`
+// What other options we might want to expose here or could be useful for users?
+
+/**
+ * Main function to run the blueprints deploy.
+ * @returns {Promise<void>}
+ */
 async function run() {
   try {
-    // @I guess if we get from the ENV, we could skip the input requirement and document that it should be set as `SANITY_AUTH_TOKEN` in GHA env?
-    const sanityToken = process.env.SANITY_TOKEN || core.getInput('sanity_token', { required: true });
+    // I guess if we get from the ENV, we could skip the input requirement and document that it should be set as `SANITY_AUTH_TOKEN` in GHA env?
+    const sanityToken = process.env.SANITY_TOKEN || core.getInput('sanity_token')
 
-    core.info('Starting Sanity blueprints deployment...');
-
-    if (!process.env.SANITY_TOKEN) {
-        // if the user didn't set this in env, we set it here
-        process.env.SANITY_AUTH_TOKEN = sanityToken;
+    if (!sanityToken) {
+      // If we don't have a token, fail the action
+      core.setFailed(
+        'Action failed: Missing the Sanity token. Please provide a valid SANITY_TOKEN as an environment variable or input `sanity_token`.',
+      )
     }
 
-    core.exportVariable('SANITY_AUTH_TOKEN', sanityToken);
+    core.info('Starting Sanity blueprints deployment...')
+    core.exportVariable('SANITY_AUTH_TOKEN', sanityToken)
 
-    // Build the command arguments
-    const args = ['blueprints', 'deploy'];
+    const args = ['blueprints', 'deploy']
 
-    core.info(`Running: sanity ${args.join(' ')}`);
-    await exec.exec('npx', ['@sanity/runtime-cli', ...args]);
+    core.info(`Running: sanity ${args.join(' ')}`)
+    await exec.exec('npx', ['@sanity/runtime-cli', ...args])
 
-    core.info('✅ Blueprints deployed successfully!');
+    core.info('✅ Blueprints deployed successfully!')
   } catch (error) {
-    core.setFailed(`Action failed: ${error.message}`);
+    core.setFailed(`Action failed: ${error.message}`)
   }
 }
 
-run();
+run()
 
 module.exports = __webpack_exports__;
 /******/ })()
--- src/index.js
+++ src/index.js
@@ -1,39 +1,41 @@
-const core = require('@actions/core');
-const exec = require('@actions/exec');
+const core = require('@actions/core')
+const exec = require('@actions/exec')
 
-// In the runtime cli it assumes the blueprints file is in the root and available.
-// Can the runtime cli take a path to the blueprints file? If so we can make that an input here too.
+// The runtime cli assumes the blueprints file is in the root and available.
+// Does the runtime cli eventually need to be extended to allow for additional vars?
 // A file path could be input if users need to define where their blueprints file is located, and then probably set the working directory too.
 
 // For now, this action will just run `runtime-cli blueprints deploy`
-// I wonder what other options we might want to expose here or could be useful for users?
+// What other options we might want to expose here or could be useful for users?
 
 /**
- * Main function to run the GitHub Action.
+ * Main function to run the blueprints deploy.
  * @returns {Promise<void>}
  */
 async function run() {
   try {
     // I guess if we get from the ENV, we could skip the input requirement and document that it should be set as `SANITY_AUTH_TOKEN` in GHA env?
-    const sanityToken = process.env.SANITY_TOKEN || core.getInput('sanity_token', { required: true });
+    const sanityToken = process.env.SANITY_TOKEN || core.getInput('sanity_token')
 
     if (!sanityToken) {
-        // If we don't have a token, fail the action
-        core.setFailed('Action failed: Missing the Sanity token. Please provide a valid SANITY_TOKEN as an environment variable or input `sanity_token`.');
+      // If we don't have a token, fail the action
+      core.setFailed(
+        'Action failed: Missing the Sanity token. Please provide a valid SANITY_TOKEN as an environment variable or input `sanity_token`.',
+      )
     }
 
-    core.info('Starting Sanity blueprints deployment...');
-    core.exportVariable('SANITY_AUTH_TOKEN', sanityToken);
+    core.info('Starting Sanity blueprints deployment...')
+    core.exportVariable('SANITY_AUTH_TOKEN', sanityToken)
 
-    const args = ['blueprints', 'deploy'];
+    const args = ['blueprints', 'deploy']
 
-    core.info(`Running: sanity ${args.join(' ')}`);
-    await exec.exec('npx', ['@sanity/runtime-cli', ...args]);
+    core.info(`Running: sanity ${args.join(' ')}`)
+    await exec.exec('npx', ['@sanity/runtime-cli', ...args])
 
-    core.info('✅ Blueprints deployed successfully!');
+    core.info('✅ Blueprints deployed successfully!')
   } catch (error) {
-    core.setFailed(`Action failed: ${error.message}`);
+    core.setFailed(`Action failed: ${error.message}`)
   }
 }
 
-run();
+run()
