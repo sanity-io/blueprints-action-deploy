635fb021078d55658b5e74d5d771fb7cacbe0f81
Stephen Fisher <stephen.fisher@sanity.io>
2025-10-29 12:57:46 -0600

    pass vars and set env variables

--- dist/index.js
+++ dist/index.js
@@ -27559,7 +27559,6 @@ const core = __nccwpck_require__(7484)
 const exec = __nccwpck_require__(5236)
 
 // The runtime cli assumes the blueprints file is in the root and available.
-// Does the runtime cli eventually need to be extended to allow for additional vars?
 // A file path could be input if users need to define where their blueprints file is located, and then probably set the working directory too.
 
 // For now, this action will just run `runtime-cli blueprints deploy`
@@ -27572,17 +27571,32 @@ const exec = __nccwpck_require__(5236)
 async function run() {
   try {
     // I guess if we get from the ENV, we could skip the input requirement and document that it should be set as `SANITY_AUTH_TOKEN` in GHA env?
-    const sanityToken = process.env.SANITY_TOKEN || core.getInput('sanity_token')
+    const sanityToken = core.getInput('sanity_token')
+    const orgId = core.getInput('organization_id')
+    const projectId = core.getInput('project_id')
+    const stackId = core.getInput('stack_id', {required: true})
 
-    if (!sanityToken) {
+    if (!sanityToken || !process.env.SANITY_AUTH_TOKEN) {
       // If we don't have a token, fail the action
-      core.setFailed(
-        'Action failed: Missing the Sanity token. Please provide a valid SANITY_TOKEN as an environment variable or input `sanity_token`.',
-      )
+      core.setFailed('Action failed: Missing a valid `sanity_token`.')
     }
 
+    if (!orgId && !projectId) {
+      // if we don't have orgId or projectId, fail the action. At least one is needed for the CLI to run deploy
+      core.setFailed('Action failed: Missing either a `project_id` or an `organization_id`.')
+    }
+
+    if (!process.env.SANITY_AUTH_TOKEN) {
+      // Set the SANITY_AUTH_TOKEN env var for the CLI to use
+      core.exportVariable('SANITY_AUTH_TOKEN', sanityToken)
+    }
+
+    // There are specifics to how the CLI interprets these variables. We can set them all. Set all three and let the CLI sort it out.
+    core.exportVariable('SANITY_ORGANIZATION_ID', orgId)
+    core.exportVariable('SANITY_PROJECT_ID', projectId)
+    core.exportVariable('SANITY_BLUEPRINT_STACK_ID', stackId)
+
     core.info('Starting Sanity blueprints deployment...')
-    core.exportVariable('SANITY_AUTH_TOKEN', sanityToken)
 
     const args = ['blueprints', 'deploy']
 
