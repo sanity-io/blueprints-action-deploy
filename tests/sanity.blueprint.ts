01af1a519a2b2c83ef687696194cb498d56bb74c
Stephen Fisher <stephen.fisher@sanity.io>
2025-12-23 14:04:26 -0700

    chore: add full scale bp test (#12)
    * kitchen sink testing
    
    * cron
    
    * update bps file and update deps
    
    * bp fixes
    
    * custom role fix
    
    * allow deletion if we need to destroy
    
    * remove success message
    
    * remove optional flag

--- tests/sanity.blueprint.ts
+++ tests/sanity.blueprint.ts
@@ -1,7 +1,15 @@
-import {defineBlueprint, defineDocumentFunction} from '@sanity/blueprints'
+import {
+    defineBlueprint,
+    defineDocumentFunction,
+    defineDocumentWebhook,
+    defineDataset,
+    defineCorsOrigin,
+    defineRole,
+} from '@sanity/blueprints'
 
 export default defineBlueprint({
     blueprintVersion: '2025-10-31',
+
     resources: [
         defineDocumentFunction({
             name: 'integration-test',
@@ -15,6 +23,50 @@ export default defineBlueprint({
                     id: `${process.env.SANITY_PROJECT_ID}.*`
                 }
             }
+        }),
+        defineDocumentWebhook({
+            name: 'test-webhook',
+            url: 'https://webhook.site/test-deploy-action',
+            on: ['create', 'update'],
+            filter: "_type == 'post'",
+            projection: '{title, _id}',
+            dataset: 'production',
+            apiVersion: 'vX',
+            project: process.env.SANITY_PROJECT_ID,
+            httpMethod: 'POST',
+            status: 'enabled',
+        }),
+        defineDataset({
+            name: 'test-dataset',
+            datasetName: 'test-dataset',
+            aclMode: 'public',
+            project: process.env.SANITY_PROJECT_ID,
+            lifecycle: {
+                deletionPolicy: 'allow',
+            },
+        }),
+        defineCorsOrigin({
+            name: 'test-cors',
+            origin: 'https://example.com',
+            allowCredentials: true,
+            project: process.env.SANITY_PROJECT_ID,
+        }),
+        defineRole({
+            name: 'custom-role',
+            title: 'Custom Role',
+            description: 'A test role',
+            appliesToUsers: true,
+            appliesToRobots: false,
+            permissions: [
+                {
+                    name: 'sanity-all-documents',
+                    action: "mode",
+                    params: {
+                        mode: "publish",
+                        history: true
+                    }
+                },
+            ],
         }),
     ],
 })
